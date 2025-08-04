4a8c084250631e66bc26f3af4327367a2c934e12
asgharale <asgharale11@gmail.com>
2025-08-04 12:43:06 +0330

    starting the project

---
+++ TodoApp/Core/Domain/TodoApp.Domain/Entities/Categories/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Domain.Entities.Items.Item;
+
+namespace TodoApp.Domain.Entities.Categories
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item()>;
+    }
+}
---
+++ TodoApp/Core/Domain/TodoApp.Domain/Entities/Items/Items.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Domain.Entities.Item
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsActive { get; set; }
+        public long CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
